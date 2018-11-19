c386343c0d01235079181620b0fbcd87d30f3349
Ron Evans <ron@hybridgroup.com>
2018-11-19 10:44:56 +0100

    build: add placeholder Go file to package root to allow for go get -d package installation
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ drivers.go
@@ -0,0 +1,4 @@
+// Package drivers is just a placeholder for the sub-packages.
+// It is here just to be able to install the package without errors using
+// go get -d github.com/ayke/tinygo-drivers
+package drivers
