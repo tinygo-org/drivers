31bc74101d702e20536548042664fe1f9ea832f7
Ron Evans <ron@hybridgroup.com>
2019-02-07 14:52:05 +0100

    examples: add example for apa102 (#17)
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ examples/apa102/main.go
@@ -0,0 +1,35 @@
+// Connects to an APA102 SPI RGB LED strip with 30 LEDS.
+package main
+
+import (
+	"image/color"
+	"machine"
+	"time"
+
+	"github.com/tinygo-org/drivers/apa102"
+)
+
+func main() {
+	machine.SPI0.Configure(machine.SPIConfig{
+		Frequency: 500000,
+		Mode:      0})
+
+	a := apa102.New(machine.SPI0)
+	leds := make([]color.RGBA, 30)
+	rg := false
+
+	for {
+		rg = !rg
+		for i := range leds {
+			rg = !rg
+			if rg {
+				leds[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0x77}
+			} else {
+				leds[i] = color.RGBA{R: 0x00, G: 0xff, B: 0x00, A: 0x77}
+			}
+		}
+
+		a.WriteColors(leds)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
