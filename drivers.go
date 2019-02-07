bb56d49ce3d87e4fb708243b3873a8c5da99fcaa
Ron Evans <ron@hybridgroup.com>
2019-02-07 09:41:24 +0100

    rename: make sure package name is consistent in all places after move to organization
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- drivers.go
+++ drivers.go
@@ -1,4 +1,4 @@
 // Package drivers is just a placeholder for the sub-packages.
 // It is here just to be able to install the package without errors using
-// go get -d github.com/ayke/tinygo-drivers
+// go get -d github.com/tinygo-org/drivers
 package drivers
