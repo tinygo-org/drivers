7dfd72c1b1066c07e22b0fd5072ff8957df9d683
Ron Evans <ron@hybridgroup.com>
2019-02-07 14:51:45 +0100

    examples: add example for mpu6050 (#16)
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ examples/mpu6050/main.go
@@ -0,0 +1,22 @@
+// Connects to an MPU6050 I2C accelerometer/gyroscope.
+package main
+
+import (
+	"machine"
+	"time"
+
+	"github.com/tinygo-org/drivers/mpu6050"
+)
+
+func main() {
+	machine.I2C0.Configure(machine.I2CConfig{})
+
+	accel := mpu6050.New(machine.I2C0)
+	accel.Configure()
+
+	for {
+		x, y, z := accel.ReadAcceleration()
+		println(x, y, z)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
