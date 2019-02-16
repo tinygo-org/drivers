89637c2727fd39e31db7a3d4fab3eab15926775f
Daniel Esteban <conejo@theninjabunny.com>
2019-02-16 13:15:17 +0100

    Basic support for VL53L1X time-of-flight distance sensor

---
+++ examples/vl53l1x/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"machine"
+
+	"time"
+
+	"github.com/tinygo-org/drivers/vl53v1x"
+)
+
+func main() {
+	machine.I2C0.Configure(machine.I2CConfig{
+		Frequency: 400000,
+	})
+	sensor := vl53l1x.New(machine.I2C0)
+	connected := sensor.Connected()
+	if !connected {
+		println("VL53L1X device not found")
+		return
+	}
+	println("VL53L1X device found")
+	sensor.Configure(true)
+	sensor.SetMeasurementTimingBudget(50000)
+	sensor.StartContinuous(50)
+	for {
+		sensor.Read(true)
+		println("Distance (mm):", sensor.Distance())
+		println("Status:", sensor.Status())
+		println("Peak signal rate (cps):", sensor.SignalRate())
+		println("Ambient rate (cps):", sensor.AmbientRate())
+		println("---")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
