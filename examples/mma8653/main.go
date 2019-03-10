b0c4f3e2cea81cf9edfa971e200939d0a4fa8589
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-03-10 22:58:45 +0100

    mma8653: return acceleration in a standard unit: micro-G.

--- examples/mma8653/main.go
+++ examples/mma8653/main.go
@@ -12,10 +12,10 @@ func main() {
 	machine.I2C0.Configure(machine.I2CConfig{})
 
 	accel := mma8653.New(machine.I2C0)
-	accel.Configure(mma8653.DataRate200Hz)
+	accel.Configure(mma8653.DataRate200Hz, mma8653.Sensitivity2G)
 
 	for {
-		x, y, z := accel.ReadAcceleration()
+		x, y, z, _ := accel.ReadAcceleration()
 		println(x, y, z)
 		time.Sleep(time.Millisecond * 100)
 	}
