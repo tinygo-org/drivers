59db5fef76e3ed4291547f68b59f8e8d6cad2460
Ron Evans <ron@hybridgroup.com>
2019-04-22 08:21:33 +0200

    thermistor: add support for thermistors such as the NTC 3950
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ examples/thermistor/main.go
@@ -0,0 +1,26 @@
+// This example uses the settings for the thermistor that is built in to the
+// Adafruit Circuit Playground Express.
+package main
+
+import (
+	"machine"
+	"time"
+
+	"github.com/tinygo-org/drivers/thermistor"
+)
+
+const ADC_PIN = machine.TEMPSENSOR
+
+func main() {
+	machine.InitADC()
+
+	sensor := thermistor.New(ADC_PIN)
+	sensor.Configure()
+
+	for {
+		temp, _ := sensor.ReadTemperature()
+		println("Temperature:", temp/1000, "ºC")
+
+		time.Sleep(2 * time.Second)
+	}
+}
