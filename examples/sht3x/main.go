af5fa39ec94b0015fa45bd8123a125cd11e58a0d
Anthony Elder <torntrousers@gmail.com>
2019-05-08 17:09:56 +0100

    Cleanup debuging

--- examples/sht3x/main.go
+++ examples/sht3x/main.go
@@ -9,20 +9,9 @@ import (
 )
 
 func main() {
-	println("Hello")
 	machine.I2C0.Configure(machine.I2CConfig{})
-	println("Hello1")
 	sensor := sht3x.New(machine.I2C0)
-	println("Hello2")
 
-	temp, humidity, _ := sensor.ReadTemperatureHumidity()
-	println("Hello3")
-	println("Temperature:", temp, "milliºC")
-	println("Relative Humidity:", humidity, "%")
-	// println(uint16(sensor.ReadTemperature()))
-	// println(uint16(sensor.ReadHumidity()))
-
-	// this doesn't work on Arduino
 	for {
 		temp, humidity, _ := sensor.ReadTemperatureHumidity()
 		t := fmt.Sprintf("%.2f", float32(temp)/1000)
