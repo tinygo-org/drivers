0bc0e29ec87063af8b8d560978a63fdd7c4286ba
Anthony Elder <torntrousers@gmail.com>
2019-05-12 20:05:21 +0100

    Update example

--- examples/ubloxGPS/main.go
+++ examples/ubloxGPS/main.go
@@ -10,15 +10,16 @@ func main() {
 	println("GPS Example2")
 	machine.I2C0.Configure(machine.I2CConfig{})
 	gps := ubloxGPS.New(machine.I2C0)
-	var f ubloxGPS.Fix
+	parser := ubloxGPS.Parser(gps)
+	var fix ubloxGPS.Fix
 	for {
-		f = gps.ReadFix()
-		if f.Valid {
-			print(f.Time)
-			print(", lat=", f.Latitude)
-			print(", long=", f.Longitude)
-			print(", altitude:=", f.Altitude)
-			print(", satelites=", f.Satelites)
+		parser.NextFix(&fix)
+		if fix.Valid {
+			print(fix.Time)
+			print(", lat=", fix.Latitude)
+			print(", long=", fix.Longitude)
+			print(", altitude:=", fix.Altitude)
+			print(", satelites=", fix.Satelites)
 			println()
 		} else {
 			println("No fix")
