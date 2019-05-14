8f28790446ced0dc5dfb8563ac12ec7d0c64a155
seph <seph@directionless.org>
2019-05-14 00:43:55 -0400

    Initial tests around rgbw
    Some initial testing. This code doesn’t seem to work on my Arduino board. Still debugging.
    
    Due to https://github.com/tinygo-org/tinygo/issues/349 the interface feels cumbersome.
    
    Until we understand how to work with white LEDs, there are a couple of interfaces. It’s all an experiment

--- ws2812/ws2812.go
+++ ws2812/ws2812.go
@@ -6,15 +6,60 @@ import (
 	"machine"
 )
 
+type ColorOrder string
+
+const (
+	RGB  ColorOrder = "RGB"
+	GRB             = "GRB"
+	GRBW            = "GRBW"
+)
+
 // Device wraps a pin object for an easy driver interface.
 type Device struct {
-	Pin machine.GPIO
+	Pin   machine.GPIO
+	order ColorOrder
+}
+
+// Opt is a functional parameter option
+type DeviceOpt func(*Device)
+
+func WithOrder(o ColorOrder) DeviceOpt {
+	return func(d *Device) {
+		d.order = o
+	}
+}
+
+type ColorOptions struct {
+	Whites []uint8
+}
+
+type ColorOpt func(*ColorOptions)
+
+func WithWhite(whites []uint8) ColorOpt {
+	return func(co *ColorOptions) {
+		co.Whites = whites
+	}
+}
+
+type ColorRGBWA struct {
+	R, G, B, W, A uint8
 }
 
 // New returns a new WS2812 driver. It does not touch the pin object: you have
 // to configure it as an output pin before calling New.
-func New(pin machine.GPIO) Device {
-	return Device{pin}
+func New(pin machine.GPIO, opts ...DeviceOpt) Device {
+	d := Device{
+		Pin: pin,
+	}
+
+	// set default color order
+	WithOrder(GRB)(&d)
+
+	for _, opt := range opts {
+		opt(&d)
+	}
+
+	return d
 }
 
 // Write the raw bitstring out using the WS2812 protocol.
@@ -26,12 +71,72 @@ func (d Device) Write(buf []byte) (n int, err error) {
 }
 
 // Write the given color slice out using the WS2812 protocol.
-// Colors are sent out in the usual GRB format.
-func (d Device) WriteColors(buf []color.RGBA) error {
-	for _, color := range buf {
-		d.WriteByte(color.G) // green
-		d.WriteByte(color.R) // red
-		d.WriteByte(color.B) // blue
+// Colors are sent out in the requested order.
+func (d Device) WriteColors(buf []color.RGBA, colorOpt ...ColorOpt) (n int, err error) {
+	co := &ColorOptions{}
+	for _, opt := range colorOpt {
+		opt(co)
 	}
-	return nil
+
+	_ = co
+	for i, c := range buf {
+		// This would probably be cleaner as a call to a function set at
+		// initialization time. But
+		// https://github.com/tinygo-org/tinygo/issues/349
+		switch d.order {
+		case RGB:
+			d.WriteByte(c.R)
+			d.WriteByte(c.G)
+			d.WriteByte(c.B)
+		case GRB:
+			d.WriteByte(c.G)
+			d.WriteByte(c.R)
+			d.WriteByte(c.B)
+		case GRBW:
+			d.WriteByte(c.G)
+			d.WriteByte(c.R)
+			d.WriteByte(c.B)
+			_ = i
+			if co.Whites != nil && i <= len(co.Whites) {
+				d.WriteByte(co.Whites[i])
+			} else {
+				d.WriteByte(0)
+			}
+		}
+
+	}
+
+	return len(buf), nil
+}
+
+// WriteRGBW writes an RGBWA pixel out.
+// TODO for 3 color pixels, this drops the white channel. It could, instead, blend it.
+func (d Device) WriteRGBW(buf []ColorRGBWA, colorOpt ...ColorOpt) (n int, err error) {
+	co := &ColorOptions{}
+	for _, opt := range colorOpt {
+		opt(co)
+	}
+
+	for _, c := range buf {
+		// This would probably be cleaner as a call to a function set at
+		// initialization time. But
+		// https://github.com/tinygo-org/tinygo/issues/349
+		switch d.order {
+		case RGB:
+			d.WriteByte(c.R)
+			d.WriteByte(c.G)
+			d.WriteByte(c.B)
+		case GRB:
+			d.WriteByte(c.G)
+			d.WriteByte(c.R)
+			d.WriteByte(c.B)
+		case GRBW:
+			d.WriteByte(c.G)
+			d.WriteByte(c.R)
+			d.WriteByte(c.B)
+			d.WriteByte(c.W)
+		}
+	}
+
+	return len(buf), nil
 }
