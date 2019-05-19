118dae1de379cb41930cf9a493f7c22afdb9c83b
Anthony Elder <torntrousers@gmail.com>
2019-05-19 15:16:53 +0100

    More WIP

--- lora/lora.go
+++ lora/lora.go
@@ -22,9 +22,9 @@ type Device struct {
 
 type Config struct {
 	Frequency       uint32
-	SpreadingFactor int8
+	SpreadingFactor uint8
 	Bandwidth       int32
-	CodingRate      int8
+	CodingRate      uint8
 	TxPower         int8
 }
 
@@ -90,6 +90,39 @@ func (d *Device) ReConfigure(cfg Config) (err error) {
 func (d *Device) SendPacket(packet []byte) {
 }
 
+func (d *Device) LastPacketRSSI() uint8 {
+	// section 5.5.5
+	var adjustValue uint8 = 157
+	if d.getFrequency() < 868000000 {
+		adjustValue = 164
+	}
+	return d.readRegister(REG_PKT_RSSI_VALUE) - adjustValue
+}
+
+func (d *Device) LastPacketSNR() uint8 {
+	return uint8(d.readRegister(REG_PKT_SNR_VALUE) / 4)
+}
+
+func (d *Device) LastPacketFrequencyError() int32 {
+	// TODO
+	// int32_t freqError = 0;
+	// freqError = static_cast<int32_t>(readRegister(REG_FREQ_ERROR_MSB) & B111);
+	// freqError <<= 8L;
+	// freqError += static_cast<int32_t>(readRegister(REG_FREQ_ERROR_MID));
+	// freqError <<= 8L;
+	// freqError += static_cast<int32_t>(readRegister(REG_FREQ_ERROR_LSB));
+	//
+	// if (readRegister(REG_FREQ_ERROR_MSB) & B1000) { // Sign bit is on
+	//    freqError -= 524288; // B1000'0000'0000'0000'0000
+	// }
+	//
+	// const float fXtal = 32E6; // FXOSC: crystal oscillator (XTAL) frequency (2.5. Chip Specification, p. 14)
+	// const float fError = ((static_cast<float>(freqError) * (1L << 24)) / fXtal) * (getSignalBandwidth() / 500000.0f); // p. 37
+	//
+	// return static_cast<long>(fError);
+	return 0
+}
+
 func (d *Device) PrintRegisters() {
 	for i := 0; i < 128; i++ {
 		fmt.Printf("%02x: %02x\n", i, d.readRegister(uint8(i)))
@@ -126,12 +159,71 @@ func (d *Device) setFrequency(frequency uint32) {
 	d.writeRegister(REG_FRF_LSB, uint8(frf>>0))
 }
 
-func (d *Device) setSpreadingFactor(spreadingFactor int8) {
+func (d *Device) getSpreadingFactor() uint8 {
+	return 0
 }
-func (d *Device) setBandwidth(bandwidth int32) {
+func (d *Device) setSpreadingFactor(spreadingFactor uint8) {
 }
-func (d *Device) setCodingRate(codingRate int8) {
+func (d *Device) getBandwidth() int32 {
+	return 0
 }
+func (d *Device) setBandwidth(sbw int32) {
+	var bw uint8
+
+	if sbw <= 7800 {
+		bw = 0
+	} else if sbw <= 10400 {
+		bw = 1
+	} else if sbw <= 15600 {
+		bw = 2
+	} else if sbw <= 20800 {
+		bw = 3
+	} else if sbw <= 312500 {
+		bw = 4
+	} else if sbw <= 41700 {
+		bw = 5
+	} else if sbw <= 62500 {
+		bw = 6
+	} else if sbw <= 125000 {
+		bw = 7
+	} else if sbw <= 250000 {
+		bw = 8
+	} else {
+		bw = 9
+	}
+
+	d.writeRegister(REG_MODEM_CONFIG_1, (d.readRegister(REG_MODEM_CONFIG_1)&0x0f)|(bw<<4))
+	d.setLdoFlag()
+}
+
+func (d *Device) setLdoFlag() {
+	// Section 4.1.1.5
+	var symbolDuration int32 = 1000 / (d.getBandwidth() / (1 << d.getSpreadingFactor()))
+
+	var config3 uint8 = d.readRegister(REG_MODEM_CONFIG_3)
+
+	// Section 4.1.1.6
+	if symbolDuration > 16 {
+		config3 = config3 | 0x08
+	} else {
+		config3 = config3 & 0xF7
+	}
+
+	d.writeRegister(REG_MODEM_CONFIG_3, config3)
+}
+
+func (d *Device) setCodingRate(denominator uint8) {
+	if denominator < 5 {
+		denominator = 5
+	} else if denominator > 8 {
+		denominator = 8
+	}
+
+	var cr = denominator - 4
+
+	d.writeRegister(REG_MODEM_CONFIG_1, (d.readRegister(REG_MODEM_CONFIG_1)&0xf1)|(cr<<1))
+}
+
 func (d *Device) setTxPower(txPower int8) {
 }
 
