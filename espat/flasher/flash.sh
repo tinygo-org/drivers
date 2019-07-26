19c95259bceab73d24b682fac3613c9f23978474
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-07-25 18:26:13 -0700

    update prebuilt firmware with BLE support

--- espat/flasher/flash.sh
+++ espat/flasher/flash.sh
@@ -16,5 +16,9 @@ bossac -i -d --port=ttyACM0 -e -w -v --offset=0x2000 -R SerialNINAPassthrough.in
 echo "Waiting for a bit..."
 sleep 1
 
+# Steps to update the firmware:
+# 1. Run `make clean` in the esp32-at directory.
+# 2. Run `make` in the esp32-at directory.
+# 3. Copy over all the .bin files somewhere in the esp32-at/build directory to this directory.
 echo "Flashing firmware to ESP32..."
-python $HOME/src/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyACM0 --baud 921600 --before no_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x10000 ota_data_initial.bin 0x1000 bootloader.bin 0x20000 at_customize.bin 0x24000 server_cert.bin 0x26000 server_key.bin 0x28000 server_ca.bin 0x2a000 client_cert.bin 0x2c000 client_key.bin 0x2e000 client_ca.bin 0x30000 factory_param.bin 0xf000 phy_init_data.bin 0x100000 esp-at.bin 0x8000 partitions_at.bin
+python $HOME/src/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyACM0 --baud 921600 --before no_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x10000 ota_data_initial.bin 0x1000 bootloader.bin 0x20000 at_customize.bin 0x21000 ble_data.bin 0x24000 server_cert.bin 0x25000 server_key.bin 0x26000 server_ca.bin 0x27000 client_cert.bin 0x28000 client_key.bin 0x29000 client_ca.bin 0x2a000 factory_param.bin 0xf000 phy_init_data.bin 0x40000 esp-at.bin 0x8000 partitions_at.bin
