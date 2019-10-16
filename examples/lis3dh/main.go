5df157230f11d3e3ed2c9e028aa16930156af899
Ron Evans <ron@hybridgroup.com>
2019-10-16 21:44:58 +0200

    lis3dh: example uses I2C1 so requires config to specify pins since they are not default
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- examples/lis3dh/main.go
+++ examples/lis3dh/main.go
@@ -11,7 +11,7 @@ import (
 var i2c = machine.I2C1
 
 func main() {
-	i2c.Configure(machine.I2CConfig{})
+	i2c.Configure(machine.I2CConfig{SCL: machine.SCL1_PIN, SDA: machine.SDA1_PIN})
 
 	accel := lis3dh.New(i2c)
 	accel.Address = lis3dh.Address1 // address on the Circuit Playground Express
