c7981f72ecae305928398bc17472bda25b2badc5
Ron Evans <ron@hybridgroup.com>
2019-10-16 22:09:53 +0200

    core: add Version string for support purposes
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ version.go
@@ -0,0 +1,5 @@
+package drivers
+
+// Version returns a user-readable string showing the version of the drivers package for support purposes.
+// Update this value before release of new version of software.
+const Version = "0.7.0-dev"
