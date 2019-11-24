25f565543a3b501ec69ff9ba031c5666dd428f91
Daniel Esteban <conejo@theninjabunny.com>
2019-11-24 21:05:19 +0100

    first version of dual steppers, not tested

--- easystepper/easystepper.go
+++ easystepper/easystepper.go
@@ -13,6 +13,11 @@ type Device struct {
 	stepNumber int32
 }
 
+// DualDevice holds information for controlling 2 motors
+type DualDevice struct {
+	devices [2]Device
+}
+
 // New returns a new easystepper driver given 4 pins numbers (not pin object),
 // number of steps and rpm
 func New(pin1, pin2, pin3, pin4 machine.Pin, steps int32, rpm int32) Device {
@@ -26,6 +31,30 @@ func New(pin1, pin2, pin3, pin4 machine.Pin, steps int32, rpm int32) Device {
 	}
 }
 
+// NewDual returns a new dual easystepper driver given 8 pins numbers (not pin object),
+// number of steps and rpm
+func NewDual(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8 machine.Pin, steps int32, rpm int32) DualDevice {
+	pin1.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin2.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin3.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin4.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin5.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin6.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin7.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	pin8.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	var dual DualDevice
+	dual.devices[0] = Device{
+		pins:      [4]machine.Pin{pin1, pin2, pin3, pin4},
+		stepDelay: 60000000 / (steps * rpm),
+	}
+	dual.devices[1] = Device{
+		pins:      [4]machine.Pin{pin5, pin6, pin7, pin8},
+		stepDelay: 60000000 / (steps * rpm),
+	}
+	return dual
+}
+
 // Move rotates the motor the number of given steps
 // (negative steps will rotate it the opposite direction)
 func (d *Device) Move(steps int32) {
@@ -37,6 +66,7 @@ func (d *Device) Move(steps int32) {
 	}
 	var stepN int8
 	var s int32
+	d.stepMotor(int8(d.stepNumber % 4))
 	for s = d.stepNumber; s < steps; s++ {
 		time.Sleep(time.Duration(d.stepDelay) * time.Microsecond)
 		if direction {
@@ -55,6 +85,60 @@ func (d *Device) Off() {
 	d.pins[1].Low()
 	d.pins[2].Low()
 	d.pins[3].Low()
+}
+
+// Move rotates the motors the number of given steps
+// (negative steps will rotate it the opposite direction)
+func (d *DualDevice) Move(stepsA, stepsB int32) {
+	min := 1
+	max := 0
+	var directions [2]bool
+	var stepN [2]int8
+	var minStep int32
+
+	directions[0] = stepsA > 0
+	directions[1] = stepsB > 0
+	if stepsA < 0 {
+		stepsA = -stepsA
+	}
+	if stepsB < 0 {
+		stepsB = -stepsB
+	}
+
+	if stepsB > stepsA {
+		stepsA, stepsB = stepsB, stepsA
+		min = 0
+		max = 1
+	}
+	d.devices[0].stepMotor(int8(d.devices[0].stepNumber % 4))
+	d.devices[1].stepMotor(int8(d.devices[1].stepNumber % 4))
+	for s := int32(0); s < stepsA; s++ {
+		time.Sleep(time.Duration(d.devices[0].stepDelay) * time.Microsecond)
+		if directions[max] {
+			stepN[max] = int8(s % 4)
+		} else {
+			stepN[max] = int8((s + 2*(s%2)) % 4)
+		}
+
+		if ((s*stepsA)/stepsB) > minStep {
+			minStep++
+			if directions[min] {
+				stepN[min] = int8(s % 4)
+			} else {
+				stepN[min] = int8((s + 2*(s%2)) % 4)
+			}
+		}
+
+		d.devices[0].stepMotor(stepN[0])
+		d.devices[1].stepMotor(stepN[1])
+	}
+
+}
+
+// Off turns off all motor pins
+func (d *DualDevice) Off() {
+	d.devices[0].Off()
+	d.devices[1].Off()
 }
 
 // stepMotor changes the pins' state to the correct step
