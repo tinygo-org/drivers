cb32d643e27f7dbf2ab71138cb831c9212e9193d
BCG <bgould@users.noreply.github.com>
2020-01-05 23:00:38 -0500

    cleaning up basic example

--- examples/ili9341/basic/main.go
+++ examples/ili9341/basic/main.go
@@ -17,48 +17,31 @@ var (
 		machine.TFT_RESET,
 		machine.TFT_RD,
 	)
-	/*
-		display = ili9341.NewSPI(
-			&machine.SPI0,
-			machine.TFT_DC,
-			machine.TFT_CS,
-			machine.NoPin,
-			machine.NoPin,
-		)
-	*/
+
+	black = color.RGBA{0, 0, 0, 255}
+	white = color.RGBA{255, 255, 255, 255}
+	red   = color.RGBA{255, 0, 0, 255}
+	blue  = color.RGBA{0, 0, 255, 255}
+	green = color.RGBA{0, 255, 0, 255}
 )
 
 func main() {
 
-	//machine.SPI0.Configure(machine.SPIConfig{Frequency: 16 * 1e6})
-
-	println("turning on backlight")
-
 	machine.TFT_BACKLIGHT.Configure(machine.PinConfig{machine.PinOutput})
 
-	println("configuring display")
 	display.Configure(ili9341.Config{})
-
-	print("width, height == ")
 	width, height := display.Size()
-	println(width, height)
-
-	black := color.RGBA{0, 0, 0, 255}
 
 	display.FillScreen(black)
 	machine.TFT_BACKLIGHT.High()
 
-	white := color.RGBA{255, 255, 255, 255}
-	red := color.RGBA{255, 0, 0, 255}
-	blue := color.RGBA{0, 0, 255, 255}
-	green := color.RGBA{0, 255, 0, 255}
 	display.FillRectangle(0, 0, width/2, height/2, white)
 	display.FillRectangle(width/2, 0, width/2, height/2, red)
 	display.FillRectangle(0, height/2, width/2, height/2, green)
 	display.FillRectangle(width/2, height/2, width/2, height/2, blue)
 	display.FillRectangle(width/4, height/4, width/2, height/2, black)
 	for {
-		time.Sleep(time.Second)
-		print(". ")
+		time.Sleep(time.Hour)
 	}
+
 }
