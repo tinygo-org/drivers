b388245195a375cea6e5d22669b6cc2009b17be3
Ron Evans <ron@hybridgroup.com>
2020-01-28 17:53:50 +0100

    ws2812: work-arounds to allow Digispark to control WS2812 LEDs
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ examples/ws2812/digispark.go
@@ -0,0 +1,10 @@
+// +build digispark
+
+package main
+
+import "machine"
+
+// This is the pin assignment for the Digispark only.
+// Replace neo in the code below to match the pin
+// that you are using if different.
+var neo machine.Pin = 0
---
+++ examples/ws2812/others.go
@@ -0,0 +1,9 @@
+// +build !digispark
+
+package main
+
+import "machine"
+
+// Replace neo in the code below to match the pin
+// that you are using if different.
+var neo machine.Pin = machine.NEOPIXELS
