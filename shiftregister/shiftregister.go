e651124b27900d736b595d6fcfb69ca1fd7ba2f8
simulot <jfc@responsiveconsulting.fr>
2020-03-03 10:23:16 +0100

    Add API for individual register's output pin

--- shiftregister/shiftregister.go
+++ shiftregister/shiftregister.go
@@ -23,14 +23,16 @@ const (
 
 // Device holds pin number
 type Device struct {
-	latch, clock, out machine.Pin
-	bits              NumberBit
+	latch, clock, out machine.Pin // IC wiring
+	bits              NumberBit   // Pin number
+	mask              uint32      // keep all pins state
 }
 
-// DeviceConfiguration for setting up the shift register
-type DeviceConfiguration struct {
-	Bits              NumberBit
-	Latch, Clock, Out machine.Pin
+// ShiftPin is the implementation of the ShiftPin interface.
+// ShiftPin provide an interface like regular machine.Pin
+type ShiftPin struct {
+	mask uint32  // Bit representing the pin
+	d    *Device // Reference to the register
 }
 
 // New returns a new shift output register device
@@ -54,6 +56,7 @@ func (d *Device) Configure() {
 // WriteMask applies mask's bits to register's outputs pin
 // mask's MSB set Q1, LSB set Q8 (for 8 bits mask)
 func (d *Device) WriteMask(mask uint32) {
+	d.mask = mask // Keep the mask for individual addressing
 	d.latch.Low()
 	for i := 0; i < int(d.bits); i++ {
 		d.clock.Low()
@@ -62,4 +65,36 @@ func (d *Device) WriteMask(mask uint32) {
 		d.clock.High()
 	}
 	d.latch.High()
+}
+
+// GetShiftPin return an individually addressable pin
+func (d *Device) GetShiftPin(pin int) *ShiftPin {
+	if pin < 0 || pin > int(d.bits) {
+		panic("invalid pin number")
+	}
+	return &ShiftPin{
+		mask: 1 << pin,
+		d:    d,
+	}
+
+}
+
+// Set changes the value of this register pin.
+func (p ShiftPin) Set(value bool) {
+	d := p.d
+	if value {
+		d.WriteMask(d.mask | p.mask)
+	} else {
+		d.WriteMask(d.mask & ^p.mask)
+	}
+}
+
+// High sets this shift register pin to high.
+func (p ShiftPin) High() {
+	p.Set(true)
+}
+
+// Low sets this shift register pin to low.
+func (p ShiftPin) Low() {
+	p.Set(false)
 }
