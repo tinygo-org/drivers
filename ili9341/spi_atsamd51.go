e5ce7b4b2ff781e213de80e9575e904bf901e5e3
sago35 <sago35@gmail.com>
2020-05-21 08:21:03 +0900

    Delete //go:inline annotations

--- ili9341/spi_atsamd51.go
+++ ili9341/spi_atsamd51.go
@@ -29,36 +29,30 @@ func NewSpi(bus machine.SPI, dc, cs, rst machine.Pin) *Device {
 func (pd *spiDriver) configure(config *Config) {
 }
 
-//go:inline
 func (pd *spiDriver) write8(b byte) {
 	pd.bus.Tx([]byte{b}, nil)
 }
 
-//go:inline
 func (pd *spiDriver) write8n(b byte, n int) {
 	for i := 0; i < n; i++ {
 		pd.write8(b)
 	}
 }
 
-//go:inline
 func (pd *spiDriver) write8sl(b []byte) {
 	pd.bus.Tx(b, nil)
 }
 
-//go:inline
 func (pd *spiDriver) write16(data uint16) {
 	pd.bus.Transfer2((data << 8) | (data >> 8))
 }
 
-//go:inline
 func (pd *spiDriver) write16n(data uint16, n int) {
 	for i := 0; i < n; i++ {
 		pd.write16(data)
 	}
 }
 
-//go:inline
 func (pd *spiDriver) write16sl(data []uint16) {
 	for i, c := 0, len(data)-2; i < c; i += 2 {
 		d := uint32((data[i+1]<<8)|(data[i+1]>>8)) << 16
