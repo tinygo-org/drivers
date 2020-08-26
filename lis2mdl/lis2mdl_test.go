29e1921f2138a9e66eb815bb1afe42df662156fb
deadprogram <ron@hybridgroup.com>
2020-08-26 22:59:35 +0200

    tester: improve API surface and implement one more test function in lis2mdl driver
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- lis2mdl/lis2mdl_test.go
+++ lis2mdl/lis2mdl_test.go
@@ -7,9 +7,26 @@ import (
 	"tinygo.org/x/drivers/tester"
 )
 
-func TestI2CAddress(t *testing.T) {
+func TestDefaultI2CAddress(t *testing.T) {
 	c := qt.New(t)
 	bus := tester.NewI2CBus(c)
 	dev := New(bus)
 	c.Assert(dev.Address, qt.Equals, uint8(MAG_ADDRESS))
+}
+
+func TestWhoAmI(t *testing.T) {
+	c := qt.New(t)
+	bus := tester.NewI2CBus(c)
+	fake := tester.NewI2CDevice(c, MAG_ADDRESS)
+	bus.AddDevice(fake)
+
+	dev := New(bus)
+
+	fake.SetupRegisters([]uint8{
+		0x4F: 0x40,
+	})
+	c.Assert(dev.Connected(), qt.Equals, true)
+
+	fake.SetupRegister(0x4F, 0x99)
+	c.Assert(dev.Connected(), qt.Equals, false)
 }
