db854df1606b1da4af8f3b3326fa073c49d7f01c
soypat <graded.sp@gmail.com>
2021-03-21 21:51:57 -0300

    cant get spi to work :(

--- enc28j60/test/main.go
+++ enc28j60/test/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"machine"
 
-	"github.com/jkaflik/tinygo-w5500-driver/wiznet/net"
 	"tinygo.org/x/drivers/enc28j60"
 )
 
@@ -12,11 +11,15 @@ SCK: PB1 == D52
 MISO(sdo): PB2 == D51
 MOSI(sdi): PB3 == D50
 CS: PB0 == D53
+	sdo:  PB2,
+	sdi:  PB3,
 */
+
 // Arduino Mega 2560 CS pin
-// var spiCS = machine.D44
-// Arduino uno CS Pin
 var spiCS = machine.D10
+
+// Arduino uno CS Pin
+// var spiCS = machine.D10
 
 // declare as global value, can't escape RAM usage
 var buff [1000]byte
@@ -24,53 +27,50 @@ var buff [1000]byte
 func main() {
 	// Inline declarations so not used as RAM
 	var (
-		// gateway or router address
-		gwAddr = net.IP{192, 168, 1, 1}
-		// IP address of ENC28J60
-		ipAddr = net.IP{192, 168, 1, 5}
-		// Hardware address of ENC28J60
-		macAddr = net.HardwareAddr{0xfe, 0xfe, 0xfe, 0x22, 0x22, 0x22}
-		// network mask
-		netmask = net.IPMask{255, 255, 255, 0}
+	// gateway or router address
+	// gwAddr = net.IP{192, 168, 1, 1}
+	// // IP address of ENC28J60
+	// ipAddr = net.IP{192, 168, 1, 5}
+	// // Hardware address of ENC28J60
+	// macAddr = net.HardwareAddr{0xfe, 0xfe, 0xfe, 0x22, 0x22, 0x22}
+	// // network mask
+	// netmask = net.IPMask{255, 255, 255, 0}
 	)
-	// enc28j60.SDB = false
+	enc28j60.SDB = true
 	// Machine-specific configuration
 	spiCS.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	spiCS.High() // prevent SPI glitches
 	machine.SPI0.Configure(machine.SPIConfig{Mode: machine.Mode0, LSBFirst: false})
-	// use pin D0 as output
-	machine.D0.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	e := enc28j60.New(spiCS, machine.SPI0)
-	// Set network specific Address
-	e.SetGatewayAddress(gwAddr)
-	e.SetIPAddress(ipAddr)
-	e.SetSubnetMask(netmask)
-	err := e.Init(buff[:], macAddr)
-	if err != nil {
-		// println(err.Error())
-		println(err.(enc28j60.ErrorCode))
-	}
-	// rv := e.GetRev()
-	// print("enc28j60 rev:")
-	// println(rv)
+	enc28j60.TestSPI(spiCS, machine.SPI0)
+}
 
-	s := e.NewSocket()
-	_ = s
-	// 0 makes a random port
-	err = s.Open("arp", 0)
-	if err != nil {
-		println(err.(enc28j60.ErrorCode))
+func printError(err error) {
+	if enc28j60.SDB {
+		println(err.Error())
+	} else {
+		println("error #", err.(enc28j60.ErrorCode))
 	}
-	// ARP protocol does not support custom payload
-	// we just wait for the destination to resolve our request
-	println("resolving")
-	gwHWAddr, err := s.Resolve()
+}
 
-	if err != nil {
-		println(err.(enc28j60.ErrorCode))
+func byteSliceToHex(b []byte) []byte {
+	o := make([]byte, len(b)*2)
+	for i := 0; i < len(b); i++ {
+		aux := byteToHex(b[i])
+		o[i*2] = aux[0]
+		o[i*2+1] = aux[1]
 	}
-	// _ = gwHWAddr
-	// // do something with gateway address
-	println(string(gwHWAddr))
-	println("finish!")
+	return o
+}
+
+func byteToHex(b byte) []byte {
+	var res [2]byte
+	res[0], res[1] = (b>>4)+'0', (b&0b0000_1111)+'0'
+	if (b >> 4) > 9 {
+		res[0] = (b >> 4) + 'A' - 10
+	}
+	if (b & 0b0000_1111) > 9 {
+		res[1] = (b & 0b0000_1111) + 'A' - 10
+	}
+	return res[:]
 }
