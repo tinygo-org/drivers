fd0301a5227ae117d98bc88ece86cf4ef1b8e8c8
sago35 <sago35@gmail.com>
2021-06-08 08:45:44 +0900

    ws2812: rename the pin to ws2812

--- examples/ws2812/others.go
+++ examples/ws2812/others.go
@@ -6,5 +6,5 @@ import "machine"
 
 // Replace neo and led in the code below to match the pin
 // that you are using if different.
-var neo machine.Pin = machine.NEOPIXELS
+var neo machine.Pin = machine.WS2812
 var led = machine.LED
