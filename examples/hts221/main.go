36455ac542203b101bc9841e7f1225ca8ef4e23f
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-08-26 23:25:32 +0800

    Create main.go

---
+++ examples/hts221/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"machine"
+	"time"
+
+	"tinygo.org/x/drivers/hts221"
+)
+
+func main() {
+
+	machine.I2C1.Configure(machine.I2CConfig{
+		SCL:       machine.P0_15, // SCL1 on Nano 33 BLE Sense
+		SDA:       machine.P0_14, // SDA1 on Nano 33 BLE Sense
+		Frequency: machine.TWI_FREQ_400KHZ,
+	})
+
+	sensor := hts221.New(machine.I2C1, hts221.ON_NANO_33_BLE)
+	// for normal sensor module, use
+	// sensor := hts221.New(machine.I2C1, hts221.STANDARD)
+
+	if !sensor.Connected() {
+		println("HTS221 not connected!")
+		return
+	}
+
+	sensor.Configure() // power on and calibrate
+
+	for {
+
+		h, _ := sensor.ReadTemperature()
+		t, _ := sensor.ReadPressure()
+		println("h =", float32(h)/1000.0, "/ t =", float32(t)/1000.0)
+		time.Sleep(time.Second)
+
+	}
+
+}
