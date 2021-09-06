ad9f41af7af613b1a9b03373b29078e8c737df1c
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-09-06 20:40:34 +0800

    Update lps22hb.go

--- lps22hb/lps22hb.go
+++ lps22hb/lps22hb.go
@@ -5,47 +5,18 @@
 package lps22hb
 
 import (
-	"machine"
-	"time"
-
 	"tinygo.org/x/drivers"
 )
 
-// Device wraps an I2C connection to a LPS22HB device.
+// Device wraps an I2C connection to a HTS221 device.
 type Device struct {
-	bus         drivers.I2C
-	Address     uint8
-	OnNano33BLE bool
-}
-
-// New creates a new LPS22HB connection. The I2C bus must already be
-// configured.
-//
-// This function only creates the Device object, it does not touch the device.
-//
-// For Nano 33 BLE Sense, use machine.P0_15 (SCL1) and machine.P0_14 (SDA1),
-// and set onNano33BLE as lps22hb.ON_NANO_33_BLE.
-func New(bus drivers.I2C, deviceType uint8) Device {
-	return Device{bus: bus, Address: LPS22HB_ADDRESS, OnNano33BLE: deviceType == 1}
+	bus     drivers.I2C
+	Address uint8
 }
 
 // Connected returns whether LPS22HB has been found.
 // It does a "who am I" request and checks the response.
 func (d *Device) Connected() bool {
-
-	// if the LPS22HB is on Nano 33 BLE Sense,
-	// turn on power pin (machine.P0_22) and I2C1 pullups power pin (machine.P1_00)
-	// and wait a moment.
-	if d.OnNano33BLE {
-		ENV := machine.Pin(22)
-		ENV.Configure(machine.PinConfig{Mode: machine.PinOutput})
-		ENV.High()
-		R := machine.Pin(32)
-		R.Configure(machine.PinConfig{Mode: machine.PinOutput})
-		R.High()
-		time.Sleep(time.Millisecond * 10)
-	}
-
 	data := []byte{0}
 	d.bus.ReadRegister(d.Address, LPS22HB_WHO_AM_I_REG, data)
 	return data[0] == 0xB1
@@ -88,20 +59,11 @@ func (d *Device) ReadTemperature() (temperature int32, err error) {
 
 // wait and trigger one shot in block update
 func (d *Device) waitForOneShot() {
-	// wait until one shot is cleared
-	data := []byte{1}
-	for {
-		d.bus.ReadRegister(d.Address, LPS22HB_CTRL2_REG, data)
-		if data[0]&0x01 == 0 {
-			break
-		}
-	}
-
 	// trigger one shot
 	d.bus.WriteRegister(d.Address, LPS22HB_CTRL2_REG, []byte{0x01})
 
 	// wait until one shot is cleared
-	data[0] = 1
+	data := []byte{1}
 	for {
 		d.bus.ReadRegister(d.Address, LPS22HB_CTRL2_REG, data)
 		if data[0]&0x01 == 0 {
