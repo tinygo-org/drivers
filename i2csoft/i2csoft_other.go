4f82c231cb0d7d977099da50580dea7503fc1bdf
sago35 <sago35@gmail.com>
2021-09-24 10:15:02 +0900

    i2csoft: add source code for each target

--- i2csoft/i2csoft_other.go
+++ i2csoft/i2csoft_other.go
@@ -1,5 +1,5 @@
-//go:build !esp32 && !atsamd51 && !atsame5x
-// +build !esp32,!atsamd51,!atsame5x
+//go:build !esp32 && !atsamd51 && !atsame5x && !stm32f4 && !rp2040 && ! nrf52840
+// +build !esp32,!atsamd51,!atsame5x,!stm32f4,!rp2040,!nrf52840
 
 package i2csoft
 
@@ -7,15 +7,9 @@ import (
 	"device"
 )
 
+// wait waits for half the time of the SCL operation interval.
 func (i2c *I2C) wait() {
-	// atsamd21 @ 48MHz
-	//  1 : about 360kHz
-	// 24 : about 96kHz
-	wait := 1
-	if i2c.baudrate < 400*1e3 {
-		wait = 19
-	}
-
+	wait := 20
 	for i := 0; i < wait; i++ {
 		device.Asm(`nop`)
 	}
