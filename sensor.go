9a64a6d6d833927db2061526c5013647c5326b03
soypat <graded.sp@gmail.com>
2021-09-26 10:08:11 -0300

    add Sensor interface

---
+++ sensor.go
@@ -0,0 +1,31 @@
+package drivers
+
+// Measurement specifies a type of measurement,
+// for example: temperature, acceleration, pressure.
+type Measurement uint32
+
+// Sensor measurements
+const (
+	Voltage Measurement = 1 << iota
+	Temperature
+	Humidity
+	Pressure
+	Distance
+	Acceleration
+	AngularVelocity
+	MagneticField
+	Luminosity
+	Time
+
+	AllMeasurements Measurement = 0xffffffff
+)
+
+// Sensor represents an object capable of making one
+// or more measurements. A sensor will then have methods
+// which read the last updated measurements.
+//
+// Many Sensors may be collected into
+// one Sensor interface to synchronize measurements.
+type Sensor interface {
+	Update(which Measurement) error
+}
