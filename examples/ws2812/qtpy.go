4f0ecf64deeb7964eb0abc7b535c6c79e2e4bc3c
sago35 <sago35@gmail.com>
2022-01-27 18:03:00 +0900

    ws2812: add support for m5stamp-c3

--- examples/ws2812/qtpy.go
+++ examples/ws2812/qtpy.go
@@ -5,11 +5,6 @@ package main
 
 import "machine"
 
-// Replace neo and led in the code below to match the pin
-// that you are using if different.
-var neo machine.Pin = machine.NEOPIXELS
-var led = machine.NoPin
-
 func init() {
 	pwr := machine.NEOPIXELS_POWER
 	pwr.Configure(machine.PinConfig{Mode: machine.PinOutput})
