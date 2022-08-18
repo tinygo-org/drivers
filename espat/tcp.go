2a2e89726135d396342beb5f4cc2497007a42be0
Arsen6331 <arsen@arsenm.dev>
2022-08-18 22:01:12 +0000

    Trim quotes from IP returned by espat's GetDNS()

--- espat/tcp.go
+++ espat/tcp.go
@@ -29,7 +29,7 @@ func (d *Device) GetDNS(domain string) (string, error) {
 		return "", errors.New("Invalid domain lookup result")
 	}
 	res := strings.Split(r[1], "\r\n")
-	return res[0], nil
+	return strings.Trim(res[0], `"`), nil
 }
 
 // ConnectTCPSocket creates a new TCP socket connection for the ESP8266/ESP32.
