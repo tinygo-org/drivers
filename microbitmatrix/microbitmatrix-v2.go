22d8a3fed0927fd8ed51b7e890d8aaffe5554e77
Lucas Bremgartner <lucas@bremis.ch>
2022-10-05 16:06:05 +0200

    microbitmatrix: add link to schema for microbit V2

--- microbitmatrix/microbitmatrix-v2.go
+++ microbitmatrix/microbitmatrix-v2.go
@@ -3,7 +3,7 @@
 
 // Package microbitmatrix implements a driver for the BBC micro:bit version 2 LED matrix.
 //
-// Schematic:
+// Schematic: https://github.com/microbit-foundation/microbit-v2-hardware/blob/main/V2.00/MicroBit_V2.0.0_S_schematic.PDF
 package microbitmatrix // import "tinygo.org/x/drivers/microbitmatrix"
 
 import (
@@ -84,7 +84,6 @@ func (d *Device) Display() error {
 			} else {
 				d.pin[5+y].Low()
 			}
-
 		}
 		time.Sleep(time.Millisecond * 4)
 	}
