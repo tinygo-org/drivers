f633a08fdb055bb387b933b73737eeb32d5ceff7
Greg Oster <oster@fween.ca>
2022-10-23 15:19:23 -0600

    First cut at merging SPI and non-SPI versions of the xpt2046 driver.

--- xpt2046/xpt2046.go
+++ xpt2046/xpt2046.go
@@ -6,11 +6,12 @@ package xpt2046
 import (
 	"machine"
 	"time"
-
+	"tinygo.org/x/drivers"
 	"tinygo.org/x/drivers/touch"
 )
 
 type Device struct {
+	bus    drivers.SPI
 	t_clk  machine.Pin
 	t_cs   machine.Pin
 	t_din  machine.Pin
@@ -31,6 +32,18 @@ func New(t_clk, t_cs, t_din, t_dout, t_irq machine.Pin) Device {
 		t_cs:      t_cs,
 		t_din:     t_din,
 		t_dout:    t_dout,
+		t_irq:     t_irq,
+	}
+}
+
+func NewSPI(bus drivers.SPI, t_cs, t_irq machine.Pin) Device {
+	t_cs.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	t_irq.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	return Device{
+		bus:       bus,
+		precision: 10,
+		t_cs:      t_cs,
 		t_irq:     t_irq,
 	}
 }
@@ -57,6 +70,55 @@ func (d *Device) Configure(config *Config) error {
 	d.readRaw() //Set Powerdown mode to enable T_IRQ
 
 	return nil
+}
+
+func (d *Device) Configure(config *Config) error {
+
+	if config.Precision == 0 {
+		d.precision = 10
+	} else {
+		d.precision = config.Precision
+	}
+
+	d.t_cs.High()
+
+	//S       = 1    --> Required Control bit
+	//A2-A0   = 000  --> nothing
+	//MODE    = 0    --> 12 bit conversion
+	//SER/DFR = 0    --> Differential preferred for pressure
+	//PD1-PD0 = 10   --> Powerdown and enable PEN_IRQ
+	d.Command(0x80) // make sure PD1 is cleared on start
+
+	return nil
+}
+
+
+// Tx and Rx pulled from st7789, modified for xps2046
+
+// Tx sends data to the touchpad
+func (d *Device) Tx(data []byte, isCommand bool) {
+	d.t_cs.Low()
+	d.bus.Tx(data, nil)
+	d.t_cs.High()
+}
+
+// Rx reads data from the touchpad
+func (d *Device) Rx(command uint8, data []byte) {
+	cmd := make([]byte,len(data))
+	cmd[0] = command
+	d.t_cs.Low()
+	d.bus.Tx(cmd,data)
+	d.t_cs.High()
+}
+
+// Command sends a command to the touch screen.
+func (d *Device) Command(command uint8) {
+	d.Tx([]byte{command}, true)
+}
+
+// Data sends data to the touch screen. // XXX needed?
+func (d *Device) Data(data uint8) {
+	d.Tx([]byte{data}, false)
 }
 
 func busSleep() {
@@ -109,7 +171,11 @@ func (d *Device) ReadTouchPoint() touch.Point {
 	d.t_cs.Low()
 
 	for ; sampleCount < d.precision && d.Touched(); sampleCount++ {
-		rx, ry, rz := d.readRaw()
+		if d.SPI != nil {
+			rx, ry, rz := d.readRawSPI()
+		} else {
+			rx, ry, rz := d.readRaw()
+		}
 		tx += uint32(rx)
 		ty += uint32(ry)
 		tz += uint32(rz)
@@ -182,6 +248,57 @@ func (d *Device) readRaw() (int32, int32, int32) {
 	}
 
 	d.t_cs.High()
+
+	//Scale X&Y to 16 bit for consistency across touch drivers
+	return int32(tx) << 4, int32(4096-ty) << 4, tz
+}
+
+
+func (d *Device) readRawSPI() (int32, int32, int32) {
+
+	data := make([]byte,4)
+
+	//S       = 1    --> Required Control bit
+	//A2-A0   = 101  --> X-Position
+	//MODE    = 0    --> 12 bit conversion
+	//SER/DFR = 0    --> Differential preferred for X,Y position
+	//PD1-PD0 = 00   --> Powerdown and enable PEN_IRQ
+
+	d.Rx(0xD0,data)
+	tx := int32((uint16(data[1])<<8 | uint16(data[2])) >>3) // 7 bits come from data[1], remaining 5 from top of data[2]
+
+	//S       = 1    --> Required Control bit
+	//A2-A0   = 001  --> Y-Position
+	//MODE    = 0    --> 12 bit conversion
+	//SER/DFR = 0    --> Differential preferred for X,Y position
+	//PD1-PD0 = 00   --> Powerdown and enable PEN_IRQ
+
+	d.Rx(0x90,data)
+	ty := int32((uint16(data[1])<<8 | uint16(data[2])) >>3) // 7 bits come from data[0], remaining 5 from top of data[1]
+
+	//S       = 1    --> Required Control bit
+	//A2-A0   = 011  --> Z1-position (pressure)
+	//MODE    = 0    --> 12 bit conversion
+	//SER/DFR = 0    --> Differential preferred for pressure
+	//PD1-PD0 = 00   --> Powerdown and enable PEN_IRQ
+
+	d.Rx(0xB0,data)
+	tz1 := int32((uint16(data[1])<<8 | uint16(data[2])) >>3) // 7 bits come from data[0], remaining 5 from top of data[1]
+
+	//S       = 1    --> Required Control bit
+	//A2-A0   = 100  --> Z2-position (pressure)
+	//MODE    = 0    --> 12 bit conversion
+	//SER/DFR = 0    --> Differential preferred for pressure
+	//PD1-PD0 = 00   --> Powerdown and enable PEN_IRQ
+
+	d.Rx(0xC0,data)
+	tz2 := int32((uint16(data[1])<<8 | uint16(data[2])) >>3) // 7 bits come from data[0], remaining 5 from top of data[1]
+
+	tz := int32(0)
+	if tz1 != 0 {
+		//Touch pressure is proportional to the ratio of z2 to z1 and the x position.
+		tz = int32(tx) * ((tz2 << 12) / (tz1 << 12))
+	}
 
 	//Scale X&Y to 16 bit for consistency across touch drivers
 	return int32(tx) << 4, int32(4096-ty) << 4, tz
