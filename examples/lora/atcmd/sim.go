c43205bd5d25f05754bb97e3365068d4f9c5db89
deadprogram <ron@hybridgroup.com>
2023-01-06 09:54:48 +0100

    examples: example with LoRa AT command set implementation
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/lora/atcmd/sim.go
@@ -0,0 +1,37 @@
+//go:build !featherwing && !gnse && !lorae5 && !nucleowl55jc
+
+package main
+
+// do simulator setup here
+func setupLora() (LoraRadio, error) {
+	return &SimLoraRadio{}, nil
+}
+
+type SimLoraRadio struct {
+}
+
+func (sr *SimLoraRadio) Reset() {
+}
+
+func (sr *SimLoraRadio) Tx(pkt []uint8, timeoutMs uint32) error {
+	return nil
+}
+
+func (sr *SimLoraRadio) Rx(timeoutMs uint32) ([]uint8, error) {
+	return nil, nil
+}
+
+func (sr *SimLoraRadio) SetFrequency(freq uint32)    {}
+func (sr *SimLoraRadio) SetIqMode(mode uint8)        {}
+func (sr *SimLoraRadio) SetCodingRate(cr uint8)      {}
+func (sr *SimLoraRadio) SetBandwidth(bw uint8)       {}
+func (sr *SimLoraRadio) SetCrc(enable bool)          {}
+func (sr *SimLoraRadio) SetSpreadingFactor(sf uint8) {}
+
+func firmwareVersion() string {
+	return "simulator " + currentVersion()
+}
+
+func lorarx() ([]byte, error) {
+	return nil, nil
+}
---
+++ examples/lora/atcmd/sx126x.go
@@ -0,0 +1,73 @@
+//go:build gnse || lorae5 || nucleowl55jc
+
+package main
+
+import (
+	"device/stm32"
+	"machine"
+	"runtime/interrupt"
+
+	rfswitch "tinygo.org/x/drivers/examples/sx126x/rfswitch"
+
+	"tinygo.org/x/drivers/lora"
+	"tinygo.org/x/drivers/sx126x"
+)
+
+const (
+	FREQ                      = 868100000
+	LORA_DEFAULT_RXTIMEOUT_MS = 1000
+	LORA_DEFAULT_TXTIMEOUT_MS = 5000
+)
+
+var (
+	loraRadio *sx126x.Device
+)
+
+// do sx126x setup here
+func setupLora() (LoraRadio, error) {
+	loraRadio = sx126x.New(machine.SPI3)
+	loraRadio.SetDeviceType(sx126x.DEVICE_TYPE_SX1262)
+
+	// Create RF Switch
+	var radioSwitch rfswitch.CustomSwitch
+	loraRadio.SetRfSwitch(radioSwitch)
+
+	if state := loraRadio.DetectDevice(); !state {
+		return nil, errRadioNotFound
+	}
+
+	// Add interrupt handler for Radio IRQs
+	intr := interrupt.New(stm32.IRQ_Radio_IRQ_Busy, radioIntHandler)
+	intr.Enable()
+
+	loraConf := lora.Config{
+		Freq:           FREQ,
+		Bw:             lora.Bandwidth_500_0,
+		Sf:             lora.SpreadingFactor9,
+		Cr:             lora.CodingRate4_7,
+		HeaderType:     lora.HeaderExplicit,
+		Preamble:       12,
+		Ldr:            lora.LowDataRateOptimizeOff,
+		Iq:             lora.IQStandard,
+		Crc:            lora.CRCOn,
+		SyncWord:       lora.SyncPrivate,
+		LoraTxPowerDBm: 20,
+	}
+
+	loraRadio.LoraConfig(loraConf)
+
+	return loraRadio, nil
+}
+
+// radioIntHandler will take care of radio interrupts
+func radioIntHandler(intr interrupt.Interrupt) {
+	loraRadio.HandleInterrupt()
+}
+
+func firmwareVersion() string {
+	return "sx126x"
+}
+
+func lorarx() ([]byte, error) {
+	return loraRadio.Rx(LORA_DEFAULT_RXTIMEOUT_MS)
+}
