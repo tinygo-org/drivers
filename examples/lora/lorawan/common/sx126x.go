854b959b3f5e14bb78044b1986cdfb82f15a7f9f
deadprogram <ron@hybridgroup.com>
2023-01-14 21:37:28 +0100

    sx126x: there is no dio0 just dio1
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/lora/lorawan/common/sx126x.go
+++ examples/lora/lorawan/common/sx126x.go
@@ -20,13 +20,13 @@ var (
 )
 
 var (
-	spi                               = machine.SPI0
-	nssPin, busyPin, dio0Pin, dio1Pin = machine.GP17, machine.GP20, machine.GP11, machine.GP10
-	rxPin, txLowPin, txHighPin        = machine.GP13, machine.GP12, machine.GP12
+	spi                        = machine.SPI0
+	nssPin, busyPin, dio1Pin   = machine.GP17, machine.GP10, machine.GP11
+	rxPin, txLowPin, txHighPin = machine.GP13, machine.GP12, machine.GP12
 )
 
 func newRadioControl() sx126x.RadioController {
-	return sx126x.NewRadioControl(nssPin, busyPin, dio0Pin, dio1Pin, rxPin, txLowPin, txHighPin)
+	return sx126x.NewRadioControl(nssPin, busyPin, dio1Pin, rxPin, txLowPin, txHighPin)
 }
 
 // do sx126x setup here
