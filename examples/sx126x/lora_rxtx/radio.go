d09ee42742b84d16eab87d8ca7fe3469dfbedd3f
deadprogram <ron@hybridgroup.com>
2023-01-15 14:47:29 +0100

    examples: set pin mappings for sx126x external board
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/sx126x/lora_rxtx/radio.go
+++ examples/sx126x/lora_rxtx/radio.go
@@ -9,11 +9,21 @@ import (
 )
 
 var (
-	spi                        = machine.SPI0
-	nssPin, busyPin, dio1Pin   = machine.GP17, machine.GP10, machine.GP11
-	rxPin, txLowPin, txHighPin = machine.GP13, machine.GP12, machine.GP12
+	spi                        = machine.SPI1
+	nssPin, busyPin, dio1Pin   = machine.GP13, machine.GP6, machine.GP7
+	rxPin, txLowPin, txHighPin = machine.GP9, machine.GP8, machine.GP8
 )
 
 func newRadioControl() sx126x.RadioController {
 	return sx126x.NewRadioControl(nssPin, busyPin, dio1Pin, rxPin, txLowPin, txHighPin)
+}
+
+func init() {
+	spi.Configure(machine.SPIConfig{
+		Mode:      0,
+		Frequency: 8 * 1e6,
+		SDO:       machine.SPI1_SDO_PIN,
+		SDI:       machine.SPI1_SDI_PIN,
+		SCK:       machine.SPI1_SCK_PIN,
+	})
 }
