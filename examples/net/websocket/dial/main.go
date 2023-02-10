9a05fc46548b68b42831667f6e9988e31308306e
Scott Feldman <sfeldma@gmail.com>
2023-02-10 00:37:24 -0800

    websocket/dial: remove work-around-code for no-RNG on rp2040

--- examples/net/websocket/dial/main.go
+++ examples/net/websocket/dial/main.go
@@ -9,7 +9,6 @@
 package main
 
 import (
-	//	"crypto/rand"
 	"fmt"
 	"log"
 	"machine"
@@ -30,35 +29,6 @@ func waitSerial() {
 		time.Sleep(100 * time.Millisecond)
 	}
 }
-
-/*
-func init() {
-	rand.Reader = &reader{}
-}
-
-type reader struct {}
-
-func (r *reader) Read(b []byte) (n int, err error) {
-	if len(b) == 0 {
-		return
-	}
-
-	var randomByte uint32
-	for i := range b {
-		if i%4 == 0 {
-			randomByte, err = machine.GetRNG()
-			if err != nil {
-				return n, err
-			}
-		} else {
-			randomByte >>= 8
-		}
-		b[i] = byte(randomByte)
-	}
-
-	return len(b), nil
-}
-*/
 
 func main() {
 	waitSerial()
