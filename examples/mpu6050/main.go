3c43af7b399698189ced5a7dfa253285b5d53291
soypat <graded.sp@gmail.com>
2023-05-27 14:58:35 -0300

    rewrite mpu6050 implementation to use drivers.Sensor interface

--- examples/mpu6050/main.go
+++ examples/mpu6050/main.go
@@ -5,18 +5,32 @@ import (
 	"machine"
 	"time"
 
+	"tinygo.org/x/drivers"
 	"tinygo.org/x/drivers/mpu6050"
 )
 
 func main() {
 	machine.I2C0.Configure(machine.I2CConfig{})
 
-	accel := mpu6050.New(machine.I2C0)
-	accel.Configure()
+	mpuDevice := mpu6050.New(machine.I2C0, mpu6050.DefaultAddress)
 
+	// Configure the device with default configuration.
+	err := mpuDevice.Configure(mpu6050.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
 	for {
-		x, y, z := accel.ReadAcceleration()
-		println(x, y, z)
 		time.Sleep(time.Millisecond * 100)
+		err := mpuDevice.Update(drivers.AllMeasurements)
+		if err != nil {
+			println("error reading from mpu6050:", err.Error())
+			continue
+		}
+		print("acceleration: ")
+		println(mpuDevice.Acceleration())
+		print("angular velocity:")
+		println(mpuDevice.AngularVelocity())
+		print("temperature celsius:")
+		println(mpuDevice.Temperature() / 1000)
 	}
 }
