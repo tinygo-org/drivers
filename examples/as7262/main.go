519222318e0b905746e9db3bd7925d79e701f56f
JohnTrunix <john@trunix.ch>
2023-08-26 12:28:36 +0200

    test(as7262): add example for as7262

---
+++ examples/as7262/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"machine"
+	"time"
+	"tinygo.org/x/drivers/as7262"
+)
+
+var (
+	i2c    = machine.I2CO
+	sensor = as7262.New(i2c)
+)
+
+func main() {
+
+	i2c.Configure(machine.I2CConfig{Frequency: machine.TWI_FREQ_100KHZ})
+	sensor.Configure()
+
+	if sensor.Connected() {
+		for {
+			// read sensor colors (will read all 6 colors)
+			colors := sensor.ReadColors()
+			fmt.Printf("Colors: %v", colors)
+			time.Sleep(time.Second)
+
+			// read sensor temperature
+			temp := sensor.ReadTemp()
+			fmt.Printf("Temperature: %d", temp)
+			time.Sleep(time.Second)
+		}
+	} else {
+		panic("as7262 not connected to I2C")
+	}
+}
