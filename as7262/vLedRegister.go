12f8e2f8c9b8e756c800444ac184995ad3ef1797
JohnTrunix <john@trunix.ch>
2023-09-20 22:42:58 +0200

    feat(as7262): init virtual led register

--- as7262/vLedRegister.go
+++ as7262/vLedRegister.go
@@ -1,11 +1,33 @@
 package as7262
 
-func (d *Device) Led(status bool) {
-	var led byte
-	if status {
-		led = 0b00000111
-	} else {
-		led = 0b00000110
+type vLedControlReg struct {
+	illuminationCurrentLimit byte
+	illuminationStatus       byte
+	indicatorCurrentLimit    byte
+	indicatorStatus          byte
+}
+
+func newVLedControlReg() *vLedControlReg {
+	return &vLedControlReg{
+		illuminationCurrentLimit: 0b00110000,
+		illuminationStatus:       0b00001000,
+		indicatorCurrentLimit:    0b00000110,
+		indicatorStatus:          0b00000001,
 	}
-	d.writeByte(LedRegister, led)
+}
+
+// encode register to a complete byte for writing
+func (r *vLedControlReg) encode() byte {
+	return r.illuminationCurrentLimit |
+		r.illuminationStatus |
+		r.indicatorCurrentLimit |
+		r.indicatorStatus
+}
+
+// decode register to represent as7262 internal state
+func (r *vLedControlReg) decode(encoded byte) {
+	r.illuminationCurrentLimit = encoded & 0b00110000
+	r.illuminationStatus = encoded & 0b00001000
+	r.indicatorCurrentLimit = encoded & 0b00000110
+	r.indicatorStatus = encoded & 0b00000001
 }
