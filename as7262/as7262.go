14750466bf9d58cb4c61220ab072fb99fe519c61
JohnTrunix <john@trunix.ch>
2023-09-26 21:31:04 +0200

    feat(as7262): add reading colors functionalities

--- as7262/as7262.go
+++ as7262/as7262.go
@@ -1,6 +1,8 @@
 package as7262
 
 import (
+	"encoding/binary"
+	"math"
 	"tinygo.org/x/drivers"
 	"tinygo.org/x/drivers/internal/legacy"
 )
@@ -40,10 +42,6 @@ func New(i2c drivers.I2C) *Device {
 		vLedControlReg: newVLedControlReg(),
 	}
 }
-
-/*
-	Internal Functions
-*/
 
 // deviceStatus returns StatusReg of as7262
 func (d *Device) deviceStatus() byte {
@@ -98,4 +96,66 @@ func (d *Device) writeByte(reg byte, value byte) {
 	}
 
 	legacy.WriteRegister(d.bus, d.Address, WriteReg, []byte{value})
+}
+
+func (d *Device) read32Bit(reg byte) float32 {
+	var bytes [4]byte
+
+	for i := 0; i < 4; i++ {
+		bytes[3-i] = d.readByte(reg + byte(i))
+	}
+	floatValue := math.Float32frombits(binary.BigEndian.Uint32(bytes[:]))
+	return floatValue
+}
+
+// Temperature returns sensor temperature
+func (d *Device) Temperature() byte {
+	return d.readByte(TempRegister)
+}
+
+// GetColors set pointer array: V, B, G, Y, O, R
+func (d *Device) GetColors(arr *[6]float32) {
+	arr[0] = d.GetViolet()
+	arr[1] = d.GetBlue()
+	arr[2] = d.GetGreen()
+	arr[3] = d.GetYellow()
+	arr[4] = d.GetOrange()
+	arr[5] = d.GetRed()
+}
+
+// GetRGB set pointer array: R, G, B
+func (d *Device) GetRGB(arr *[3]float32) {
+	arr[0] = d.GetRed()
+	arr[1] = d.GetGreen()
+	arr[2] = d.GetBlue()
+}
+
+// GetViolet returns violet value
+func (d *Device) GetViolet() float32 {
+	return d.read32Bit(VCalReg)
+}
+
+// GetBlue returns blue value
+func (d *Device) GetBlue() float32 {
+	return d.read32Bit(BCalReg)
+}
+
+// GetGreen returns green value
+func (d *Device) GetGreen() float32 {
+	return d.read32Bit(GCalReg)
+}
+
+// GetYellow returns yellow value
+func (d *Device) GetYellow() float32 {
+	return d.read32Bit(YCalReg)
+}
+
+// GetOrange returns orange value
+func (d *Device) GetOrange() float32 {
+	return d.read32Bit(OCalReg)
+}
+
+// GetRed returns red value
+func (d *Device) GetRed() float32 {
+	return d.read32Bit(RCalReg)
 }
