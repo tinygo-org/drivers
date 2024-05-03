5eeba138a48b27accf118270c18b21a7632a92c0
sago35 <sago35@gmail.com>
2024-05-03 10:31:39 +0900

    Simplify examples/ws2812

--- examples/ws2812/arduino.go
+++ examples/ws2812/arduino.go
@@ -4,7 +4,6 @@ package main
 
 import "machine"
 
-// Replace neo and led in the code below to match the pin
+// Replace neo in the code below to match the pin
 // that you are using if different.
 var neo = machine.D2
-var led = machine.LED
