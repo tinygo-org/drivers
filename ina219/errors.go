08564d0e3f519a38adf8cde0bf83207635a6dd55
Warren Guy <5602790+warrenguy@users.noreply.github.com>
2024-09-25 05:45:33 +0100

    add INA219 driver

---
+++ ina219/errors.go
@@ -0,0 +1,9 @@
+package ina219
+
+type ErrOverflow struct{}
+
+func (e ErrOverflow) Error() string { return "overflow" }
+
+type ErrNotReady struct{}
+
+func (e ErrNotReady) Error() string { return "not ready" }
