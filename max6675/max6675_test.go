898be11bf7328d36befd0cb6eccf556ffe7853a6
John Hobbs <john@velvetcache.org>
2025-03-12 22:02:53 -0500

    Expectation based Pin test helper

--- max6675/max6675_test.go
+++ max6675/max6675_test.go
@@ -11,17 +11,20 @@ import (
 func Test_MAX6675_Read(t *testing.T) {
 	c := qt.New(t)
 	spi := tester.NewSPIBus(c)
+	cs := tester.NewPin(c)
 
 	var expectedCelsius float32 = 20.25
 
 	temp := uint16(expectedCelsius/0.25) << 3
 
+	cs.ExpectSet(false)
 	spi.Expect(
 		[]byte{0, 0},
 		[]byte{byte(temp >> 8), byte(temp)},
 	)
+	cs.ExpectSet(true)
 
-	dev := max6675.NewDevice(spi, tester.NewNoopPin())
+	dev := max6675.NewDevice(spi, cs)
 
 	actual, err := dev.Read()
 	c.Assert(err, qt.Equals, nil)
--- tester/pin.go
+++ tester/pin.go
@@ -1,11 +1,64 @@
 package tester
 
+import "tinygo.org/x/drivers"
+
+var _ drivers.Pin = (*Pin)(nil)
+var _ drivers.Pin = (*NoopPin)(nil)
+
+type pinExpectation struct {
+	get   bool
+	value bool
+}
+
 type Pin struct {
-	c Failer
+	c            Failer
+	expectations []pinExpectation
+}
+
+func (p *Pin) ExpectGet(high bool) {
+	p.expectations = append(p.expectations, pinExpectation{get: true, value: high})
+}
+
+func (p *Pin) ExpectSet(high bool) {
+	p.expectations = append(p.expectations, pinExpectation{get: false, value: high})
+}
+
+func (p *Pin) Get() bool {
+	if len(p.expectations) == 0 {
+		p.c.Fatalf("unexpected pin read")
+	}
+	ex := p.expectations[0]
+	if !ex.get {
+		p.c.Fatalf("unexpected pin read")
+	}
+	p.expectations = p.expectations[1:]
+	return ex.value
+}
+
+func (p *Pin) Set(high bool) {
+	if len(p.expectations) == 0 {
+		p.c.Fatalf("unexpected pin write")
+	}
+	ex := p.expectations[0]
+	if ex.get {
+		p.c.Fatalf("unexpected pin write")
+	}
+	if ex.value != high {
+		p.c.Fatalf("unexpected pin write: got %v, expecting %v", high, ex.value)
+	}
+	p.expectations = p.expectations[1:]
+}
+
+func (p *Pin) High() {
+	p.Set(true)
+}
+
+func (p *Pin) Low() {
+	p.Set(false)
 }
 
 func NewPin(c Failer) *Pin {
-	return &Pin{c}
+	return &Pin{c, []pinExpectation{}}
 }
 
 type NoopPin struct{}
