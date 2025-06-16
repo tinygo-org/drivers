cfa9206c1725dcb968eeaff8b67ebcb21c4dc6b9
Ayke van Laethem <aykevanlaethem@gmail.com>
2025-06-16 17:10:53 +0200

    ws2812: add RP2350 support
    Adding 150MHz support for the RP2350

--- ws2812/ws2812_cortexm.go
+++ ws2812/ws2812_cortexm.go
@@ -31,6 +31,9 @@ func (d Device) WriteByte(c byte) error {
 	case 125_000_000: // 125 MHz e.g. rp2040 originally
 		d.writeByte125(c)
 		return nil
+	case 150_000_000: // 150MHz, e.g. rp2350
+		d.writeByte150(c)
+		return nil
 	case 168_000_000: // 168MHz, e.g. stm32f405
 		d.writeByte168(c)
 		return nil
