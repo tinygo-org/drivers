be109e2f18f7efea96c820becd5d52f9c1776c93
Yurii Soldak <ysoldak@gmail.com>
2025-11-12 10:09:17 +0100

    avoid leaks in i2c legacy helper functions

--- internal/legacy/i2clegacy.go
+++ internal/legacy/i2clegacy.go
@@ -2,13 +2,18 @@ package legacy
 
 import "tinygo.org/x/drivers"
 
+var i2cBuf []uint8 = make([]uint8, 1)
+
 func ReadRegister(bus drivers.I2C, addr uint8, reg uint8, data []byte) error {
-	return bus.Tx(uint16(addr), []byte{reg}, data)
+	i2cBuf[0] = reg
+	return bus.Tx(uint16(addr), i2cBuf[:1], data)
 }
 
 func WriteRegister(bus drivers.I2C, addr uint8, reg uint8, data []byte) error {
-	buf := make([]uint8, len(data)+1)
-	buf[0] = reg
-	copy(buf[1:], data)
-	return bus.Tx(uint16(addr), buf, nil)
+	if len(i2cBuf) < 1+len(data) {
+		i2cBuf = make([]uint8, 1+len(data))
+	}
+	i2cBuf[0] = reg
+	copy(i2cBuf[1:], data)
+	return bus.Tx(uint16(addr), i2cBuf[:1+len(data)], nil)
 }
