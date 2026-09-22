d8f37ecf60ea134f1f54897c02a4b317be8c3ffc
Navendu Pottekkat <navendu@apache.org>
2026-09-22 12:03:19 +0530

    irremote: widen the 562.5us mark windows to 750us
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

--- irremote/receiver.go
+++ irremote/receiver.go
@@ -138,7 +138,7 @@ func (ir *ReceiverDevice) pinChange(pin machine.Pin) {
 			}
 		}
 	case bit_read_start:
-		if duration > time.Microsecond*700 || duration < time.Microsecond*400 {
+		if duration > time.Microsecond*750 || duration < time.Microsecond*400 {
 			// Invalid interval for 562.5µs pulse. Reset
 			ir.resetStateMachine()
 		} else {
@@ -170,7 +170,7 @@ func (ir *ReceiverDevice) pinChange(pin machine.Pin) {
 			}
 		}
 	case trail_pulse_end:
-		if duration > time.Microsecond*700 || duration < time.Microsecond*400 {
+		if duration > time.Microsecond*750 || duration < time.Microsecond*400 {
 			// Invalid interval for trailing 562.5µs pulse. Reset
 			ir.resetStateMachine()
 		} else {
